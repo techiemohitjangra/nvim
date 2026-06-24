9c4e97399db4fb0360233bf97f1ce55edc6649ef
Mohit Jangra <mohitjangra12@gmail.com>
2026-06-25 03:04:38 +0530

    updated options and added some new options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,47 +1,53 @@
 -- vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'noselect' }
 
-vim.o.number             = true
-vim.o.numberwidth        = 3
-vim.o.relativenumber     = true
+vim.opt.number           = true
+vim.opt.numberwidth      = 3
+vim.opt.relativenumber   = true
 
-vim.o.wrap               = false
+vim.opt.wrap             = false
 
-vim.o.tabstop            = 4
-vim.o.softtabstop        = 4
-vim.o.shiftwidth         = 4
-vim.o.expandtab          = true
+vim.opt.tabstop          = 4
+vim.opt.softtabstop      = 4
+vim.opt.shiftwidth       = 4
+vim.opt.expandtab        = true
 
-vim.o.smartindent        = true
+vim.opt.smartindent      = true
 
-vim.o.swapfile           = false
-vim.o.backup             = false
-vim.o.undodir            = os.getenv('HOME') .. '/.vim/undodir'
-vim.o.undofile           = true
+vim.opt.swapfile         = false
+vim.opt.backup           = false
+vim.opt.undodir          = os.getenv('HOME') .. '/.vim/undodir'
+vim.opt.undofile         = true
 
-vim.o.hlsearch           = false
-vim.o.incsearch          = true
+vim.opt.hlsearch         = false
+vim.opt.incsearch        = true
+vim.opt.inccommand       = 'split'
+vim.opt.laststatus       = 3
 
-vim.o.termguicolors      = true
+vim.opt.splitbelow       = true
+vim.opt.splitright       = true
+
+vim.opt.termguicolors    = true
 
 vim.opt.scrolloff        = 10
+vim.opt.colorcolumn      = '80'
 vim.opt.signcolumn       = 'yes'
--- vim.o.isfname:append('@-@')
 
-vim.o.updatetime         = 50
-
-vim.o.colorcolumn        = '80'
+vim.opt.updatetime       = 50
 
 -- Spell checking
-vim.o.spell              = true
-vim.o.spelllang          = 'en_us'
+vim.opt.spell            = true
+vim.opt.spelllang        = 'en_us'
 
 -- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase         = true
-vim.o.smartcase          = true
+vim.opt.ignorecase       = true
+vim.opt.smartcase        = true
 
-vim.o.completeopt        = 'menuone,noselect'
+vim.opt.completeopt      = 'menuone,noselect'
 
 -- netrw config
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner       = 0
 vim.g.netrw_winsize      = 25
+
+vim.opt.isfname:append('@-@')
+vim.opt.clipboard:append('unnamedplus')
